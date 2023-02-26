ce6e28f609d08795979e3dde0a73a51969888554
zhunan <2945556214@qq.com>
2023-02-26 11:56:41 +0800

    热映电影列表

--- src/vant.config.js
+++ src/vant.config.js
@@ -1,19 +1,29 @@
 import {
     Button,
+    Cell,
+    CellGroup,
     Icon,
     Tab,
     Tabs,
     Tabbar,
-    TabbarItem
+    TabbarItem,
+    Image as VanImage,
+    Lazyload,
+    List
 } from 'vant';
 
 export default {
     install(Vue) {
         Vue.use(Button);
+        Vue.use(Cell);
+        Vue.use(CellGroup);
+        Vue.use(List);
+        Vue.use(Lazyload);
         Vue.use(Icon);
         Vue.use(Tab);
         Vue.use(Tabs);
         Vue.use(Tabbar);
         Vue.use(TabbarItem);
+        Vue.use(VanImage);
     }
 }
